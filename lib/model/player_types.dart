790fd2aa9fea5832d5daa896d9cd209b7bffd535
JVDS17 <65094319+JVDS17@users.noreply.github.com>
2022-12-06 19:53:14 -0300

    api teste
    Adicionado as classes da api e uma tentativa de chamada

---
+++ lib/model/player_types.dart
@@ -0,0 +1,33 @@
+  
+  class PlayerTypes{
+    String image;
+    String nickname;
+    String name;
+    String age;
+    String rating;
+    String impact;
+    String dpr;
+    String adr;
+    String kast;
+    String kpr;
+    String headshots;
+    String mapsPlayed;
+
+    PlayerTypes({
+      this.image,
+      this.nickname,
+      this.name,
+      this.age,
+      this.rating,
+      this.impact,
+      this.dpr,
+      this.adr,
+      this.kast,
+      this.kpr,
+      this.headshots,
+      this.mapsPlayed
+    });
+  }
+  
+  
+  
---
+++ lib/model/team_model.dart
@@ -0,0 +1,15 @@
+class TeamModel{
+  String id;
+  String ranking;
+  String name;
+  List<PlayerTypes> types;
+
+  TeamModel({
+    this.id,
+    this.ranking,
+    this.name,
+  });
+
+}
+
+
