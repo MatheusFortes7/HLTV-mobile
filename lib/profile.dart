819de8365c9522e8ac62a4d11a6ee671e59f758d
JVDS17 <65094319+JVDS17@users.noreply.github.com>
2022-12-11 00:35:50 -0300

    Update profile.dart
    criação do perfil

--- lib/profile.dart
+++ lib/profile.dart
@@ -11,17 +11,71 @@ class Profile extends StatefulWidget {
 class _Profile extends State<Profile> {
   
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Perfil"),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: Column(
-          
+Widget profile() {
+  return new Scaffold(body:
+    Column(
+      children: [
+        new Expanded(child:
+        Container(
+          height: 230,
+          decoration: BoxDecoration(
+            color: Colors.lightBlue,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.elliptical(200, 40),
+                bottomRight: Radius.elliptical(200, 40)
+            ),
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Perfil",
+              style: GoogleFonts.varelaRound(fontSize: 60, color: Colors.white),
+            ),
+          ),
         ),
-      ),
-    );
+          flex: 1,
+        ),
+        Transform(
+          transform: Matrix4.translationValues(0, -70, 0),
+          child: Stack(
+            children: [
+                  Container(
+                    height: 120,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.person, size: 100),
+                  ),
+            ],),
+        ),
+        listTile(Icons.mail, "julio@gmail.com"),
+        listTile(Icons.password, "********"),
+      ],
+    ),
+  );
+}
+Widget listTile(IconData icon, String title) {
+  return Padding(
+      padding: const EdgeInsets.fromLTRB(50,0,50,30),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: ListTile(
+          tileColor: Colors.white,
+          leading:
+          Icon(
+            icon,
+            color: Colors.teal,),
+          title: Text(title),
+          trailing:
+            Icon(
+              Icons.edit,
+              color: Colors.yellow,
+            ),
+        ),
+      )
+  );
+}
   }
 }
